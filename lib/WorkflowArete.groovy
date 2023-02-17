300f52429ee9cee5e056536aec4e4905a6111b14
João Vitor <jvfecav@gmail.com>
2023-02-17 11:03:27 -0300

    feat: Add initialization script to the workflow (#40)
    * refactor: Add initialization function
    
    * refactor: Add initialization in main.nf
    
    * chore: Add multiqc_logo to schema
    
    Signed-off-by: jvfe <jvfecav@gmail.com>
    
    ---------
    
    Signed-off-by: jvfe <jvfecav@gmail.com>

--- lib/WorkflowArete.groovy
+++ lib/WorkflowArete.groovy
@@ -5,20 +5,74 @@
 class WorkflowArete {
 
     //
-    // Check and validate parameters
+    // Citation string for pipeline
     //
+    public static String citation(workflow) {
+        return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
+            // TODO nf-core: Add Zenodo DOI for pipeline after first release
+            //"* The pipeline\n" +
+            //"  https://doi.org/10.5281/zenodo.XXXXXXX\n\n" +
+            "* The nf-core framework\n" +
+            "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
+            "* Software dependencies\n" +
+            "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
+    }
 
-    /*
-    e.g force a param to exist
-    public static void initialise(params, log) {
-        genomeExistsError(params, log)
+    //
+    // Print help to screen if required
+    //
+    public static String help(workflow, params, log) {
+        def command = "nextflow run ${workflow.manifest.name} --input_sample_table samplesheet.csv --outdir arete_results -profile docker"
+        def help_string = ''
+        help_string += NfcoreSchema.paramsHelp(workflow, params, command)
+        help_string += '\n' + citation(workflow) + '\n'
+        help_string += NfcoreTemplate.dashedLine(params.monochrome_logs)
+        return help_string
+    }
 
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+    //
+    // Print parameter summary log to screen
+    //
+    public static String paramsSummaryLog(workflow, params, log) {
+        def summary_log = ''
+        summary_log += NfcoreSchema.paramsSummaryLog(workflow, params)
+        summary_log += NfcoreTemplate.dashedLine(params.monochrome_logs)
+        return summary_log
+    }
+
+    //
+    // Validate parameters and print summary to screen
+    //
+    public static void initialise(workflow, params, log) {
+        // Print help to screen if required
+        if (params.help) {
+            log.info help(workflow, params, log)
+            System.exit(0)
+        }
+
+        // Validate workflow parameters via the JSON schema
+        if (params.validate_params) {
+            NfcoreSchema.validateParameters(workflow, params, log)
+        }
+
+        // Print parameter summary log to screen
+        log.info paramsSummaryLog(workflow, params, log)
+
+        // Check that conda channels are set-up correctly
+        if (params.enable_conda) {
+            Utils.checkCondaChannels(log)
+        }
+
+        // Check the hostnames against configured profiles
+        NfcoreTemplate.hostName(workflow, params, log)
+
+        // Check input has been provided
+        if (!params.input_sample_table) {
+            log.error "Please provide an input samplesheet to the pipeline e.g. '--input_sample_table samplesheet.csv'"
             System.exit(1)
         }
     }
-    */
+
     //
     // Get workflow summary for MultiQC
     //
