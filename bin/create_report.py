c7e41cff5d7e1f91915b9523b24d4e59dce68469
jvfe <jvfecav@gmail.com>
2023-05-02 08:47:09 -0300

    refactor: Fix column when using prokka

--- bin/create_report.py
+++ bin/create_report.py
@@ -58,10 +58,7 @@ def create_report(ann, diamond_outs, rgi, mobsuite):
 
     # RGI output
     rgi_df = read_table(rgi)
-    rgi_sum = rgi_df[
-        (rgi_df["Best_Identities"] > 80)
-        & (rgi_df["Percentage_Length_of_Reference_Sequence"] > 80)
-    ]
+    rgi_sum = rgi_df[rgi_df["Best_Identities"] > 80]
     rgi_sum = rgi_sum[["Contig", "Best_Hit_ARO", "Cut_Off", "genome_id"]].rename(
         columns={"Contig": "orf", "Best_Hit_ARO": "AMR", "Cut_Off": "rgi_cutoff"}
     )
