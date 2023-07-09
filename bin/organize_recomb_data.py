054703d13d82ea1047e0e51ffc3ad053518faf7c
jvfe <jvfecav@gmail.com>
2023-07-09 12:20:30 -0300

    feat: Add module to create recombination samplesheet

---
+++ bin/organize_recomb_data.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import argparse
+from pandas import read_table, read_csv, merge, DataFrame
+from pathlib import Path
+
+
+def parse_args(args=None):
+    Description = "Create recombination workflow input data"
+    Epilog = "Example usage: python organize_recomb_data.py <QUAST_REPORT> <POPPUNK_CLUSTERS> <ASSEMBLIES> <FILE_OUT>"
+
+    parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+    parser.add_argument("QUAST_REPORT", help="QUAST QC report.")
+    parser.add_argument(
+        "POPPUNK_CLUSTERS", help="Genomes and the clusters they belong to."
+    )
+    parser.add_argument("ASSEMBLIES", help="Assembly samplesheet.")
+    parser.add_argument("FILE_OUT", help="Output file.")
+    return parser.parse_args(args)
+
+
+def get_cluster_samplesheet(df, cluster):
+    out_path = Path(f"cluster_{cluster}.txt").resolve()
+    subset = df.loc[df["Cluster"] == cluster, ["id", "path"]]
+    subset["path"] = [Path(path).name for path in subset["path"].to_list()]
+
+    subset.to_csv(out_path, sep="\t", index=False, header=False)
+
+    return str(out_path)
+
+
+def create_recomb_input(quast_report, poppunk_clusters, assembly_samplesheet, file_out):
+    # Parsing datasets
+    quast = read_table(quast_report).loc[:, ["Assembly", "N50"]]
+    quast["Assembly"] = quast["Assembly"].str.replace("_T1.*", "", regex=True)
+
+    poppunk = read_csv(poppunk_clusters)
+    poppunk["Taxon"] = poppunk["Taxon"].str.replace("_T1.*", "", regex=True)
+
+    assemblies = read_csv(assembly_samplesheet, names=["id", "assembly_path"])
+    assemblies["assembly_path"] = [
+        Path(path).name for path in assemblies["assembly_path"].to_list()
+    ]
+
+    # Merging datasets
+    merged = poppunk.merge(quast, right_on="Assembly", left_on="Taxon").loc[
+        :, ["Cluster", "Assembly", "N50"]
+    ]
+    df_highest_n50 = (
+        merged.sort_values("N50", ascending=False)
+        .groupby("Cluster")
+        .first()
+        .reset_index()
+    )
+    df_highest_n50
+    df_highest_n50.rename(columns={"Assembly": "Reference"}, inplace=True)
+    df_merged = merge(
+        merged, df_highest_n50[["Cluster", "Reference"]], on="Cluster", how="left"
+    )
+    df_with_assemblies = (
+        merge(assemblies, df_merged, left_on="id", right_on="Assembly")
+        .merge(
+            assemblies,
+            right_on="id",
+            left_on="Reference",
+        )
+        .loc[
+            :,
+            ["Cluster", "Assembly", "assembly_path_x", "Reference", "assembly_path_y"],
+        ]
+        .rename(
+            columns={
+                "Assembly": "id",
+                "assembly_path_x": "path",
+                "assembly_path_y": "reference_path",
+            }
+        )
+    )
+
+    # Getting cluster samplesheets
+    clusters = df_with_assemblies["Cluster"].unique()
+    cluster_sheets = [
+        get_cluster_samplesheet(df_with_assemblies, cluster) for cluster in clusters
+    ]
+    cluster_ids = [
+        int(re.findall(r"cluster_(\d+)", cluster_path)[0])
+        for cluster_path in cluster_sheets
+    ]
+    cluster_df = DataFrame({"Cluster": cluster_ids, "samplesheet": cluster_sheets})
+
+    # Writing final dataframe
+    final_df = cluster_df.merge(
+        df_with_assemblies[["Cluster", "Reference", "reference_path"]], on="Cluster"
+    ).drop_duplicates()
+
+    final_df.to_csv(file_out, index=False)
+
+
+def main(args=None):
+    args = parse_args(args)
+    create_recomb_input(
+        args.QUAST_REPORT, args.POPPUNK_CLUSTERS, args.ASSEMBLIES, args.FILE_OUT
+    )
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ recombination.nf
@@ -0,0 +1,57 @@
+include { GET_RECOMB_INPUT } from './modules/local/get_recomb_input'
+
+
+workflow {
+
+    // take:
+    //     assemblies
+    //     poppunk_clusters
+    //     quast_report
+
+    main:
+
+        poppunk_clusters = file(params.poppunk_csv)
+        quast_report = file(params.quast_report)
+
+        assemblies = Channel.of(
+            [[id:'SRR14022735'], file("${baseDir}/test/SRR14022735_T1.scaffolds.fa")],
+            [[id:'SRR14022764'], file("${baseDir}/test/SRR14022764_T1.scaffolds.fa")],
+            [[id:'SRR14022737'], file("${baseDir}/test/SRR14022737_T1.scaffolds.fa")],
+            [[id:'SRR14022754'], file("${baseDir}/test/SRR14022754_T1.scaffolds.fa")]
+        )
+
+        assemblies
+            .collectFile(newLine: true) { item ->
+                [ "${item[0].id}.txt", item[0].id + ',' + item[1].toString() ]
+            }
+            .collectFile(name: 'assembly_samplesheet.tsv')
+            .set { assembly_samplesheet }
+
+        GET_RECOMB_INPUT (
+            quast_report,
+            poppunk_clusters,
+            assembly_samplesheet
+        )
+
+        GET_RECOMB_INPUT.out.csv
+            .splitCsv(header: true)
+            .map { row -> tuple(row.Cluster, file(row.samplesheet), row.Reference, file(row.reference_path)) }
+            .set { recomb_input }
+
+        assemblies
+            .combine (recomb_input.map { c,s,ref,reffile -> ref } | collect | map { [it] })
+            .filter { meta, path, to_remove -> !(meta["id"] in to_remove) }
+            .map { it[0, 1] }
+            .collect { meta, path -> path }
+            .set { unduplicated_assemblies }
+
+        // SKA (
+        //     ska_input.map { c, s, r, rf -> tuple(c,s,rf) },
+        //     unduplicated_assemblies
+        //  )
+
+
+
+    // emit:
+    //     genomes_to_remove = genomes_to_remove
+}
